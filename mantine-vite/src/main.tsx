3f1e8c72c19fbbb8c4ac76645a6f96b95973d05c
Jiawen Geng <technicalcute@gmail.com>
2023-07-05 07:18:23 +0000

    fix: router highlight

--- mantine-vite/src/main.tsx
+++ mantine-vite/src/main.tsx
@@ -2,9 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { MantineProvider } from '@mantine/core'
+import { Notifications } from '@mantine/notifications'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+      <MantineProvider withNormalizeCSS withGlobalStyles>
+      <Notifications />
+      <App />
+    </MantineProvider>
   </React.StrictMode>,
 )
